88de577050cf6f8658fe35f96d063b2cf5986e1c
Ayberk Canturk <ayberk.canturk@gmail.com>
2022-10-23 14:48:34 +0100

    * Exception handling is improved in Worker

--- src/Petroineos.CodingChallenge.IntradayReport.WindowsService/Worker.cs
+++ src/Petroineos.CodingChallenge.IntradayReport.WindowsService/Worker.cs
@@ -26,8 +26,29 @@ namespace Petroineos.CodingChallenge.IntradayReport.WindowsService
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
-                    await Task.WhenAll(ExecuteWithWaitAndRetry(cancellationToken),
-                         Task.Delay(TimeSpan.FromSeconds(_workerServiceOptionsMonitor.CurrentValue.TaskDelayInSeconds), cancellationToken));
+                    var mainTask = ExecuteWithWaitAndRetry(cancellationToken);
+                    var delayerTask = Task.Delay(TimeSpan.FromSeconds(_workerServiceOptionsMonitor.CurrentValue.TaskDelayInSeconds), cancellationToken);
+
+                    try
+                    {
+                        await Task.WhenAll(mainTask, delayerTask);
+                    }
+                    catch (AggregateException ae)
+                    {
+                        var baseException = ae.GetBaseException();
+
+                        foreach (var e in ae.Flatten().InnerExceptions)
+                        {
+                            var message = e.Message;
+
+                            if (baseException == e)
+                            {
+                                message = $"Base Exception: {message}";
+                            }
+
+                            _logger.LogError(e, "{Message}", message);
+                        }
+                    }
                 }
             }
             catch (Exception ex)
@@ -43,10 +64,9 @@ namespace Petroineos.CodingChallenge.IntradayReport.WindowsService
             var waitAndRetry = Policy
                 .Handle<Exception>()
                 .WaitAndRetryAsync(
-                    retryCount: _workerServiceOptionsMonitor.CurrentValue.NumOfRetries,
-                    sleepDurationProvider: retryAttempt =>
-                        TimeSpan.FromSeconds(60 * Math.Pow(2, retryAttempt - 1)),
-                    onRetry: (ex, timeSpan, retryAttempt, context) =>
+                    _workerServiceOptionsMonitor.CurrentValue.NumOfRetries,
+                    retryAttempt => TimeSpan.FromSeconds(60 * Math.Pow(2, retryAttempt)),
+                    (ex, timeSpan, retryAttempt, context) =>
                     {
                         _logger.LogError(ex, "Retry attempt: {retryAttempt}. Operation failed: {ex.Message}", retryAttempt, ex.Message);
                     }
